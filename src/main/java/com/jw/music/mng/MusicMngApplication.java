e9cc2a6af9a712abfbd0df48be5d1c7c279fff79
james.guo <james.guo@ly.com>
2022-03-20 21:26:16 +0800

    init

---
+++ src/main/java/com/jw/music/mng/MusicMngApplication.java
@@ -0,0 +1,13 @@
+package com.jw.music.mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicMngApplication.class, args);
+    }
+
+}
